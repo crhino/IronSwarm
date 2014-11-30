235f27c07abb03b8962aea712caefe3b7e7eb497
crhino <piraino.chris@gmail.com>
2014-11-30 11:20:29 -0800

    Move byteid and agent to top-level and add artifact.
    The main work being done by this framework will be in
    the swarm module, agent/artifact/etc are conceived of as mostly just
    interfaces at the moment.

---
+++ src/artifact.rs
@@ -0,0 +1,6 @@
+// Artifact in the Swarm Ecosystem
+//
+// An Artifact is a resource that Swarm Agents can interact with. Artifacts are
+// discovered by Agents locally and knowledge of Artifacts is dispersed throughout
+// the Swarm using either the INFO or BROADCAST RPC, depending on the associated
+// importance of a particular Artifact.
